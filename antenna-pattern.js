0662b1f9a2dac03c96c0cefd072b42347fd19617
enukane <enukane@glenda9.org>
2023-04-26 22:51:47 +0900

    antenna-pattern update
    Signed-off-by: enukane <enukane@glenda9.org>

--- antenna-pattern.js
+++ antenna-pattern.js
@@ -26,6 +26,8 @@ var __edge_point = {
     db: null
 }
 
+var __current_deg = 0
+
 var __selected_points = []
 
 function redraw_canvas() {
@@ -77,8 +79,8 @@ function draw_selected_points(points) {
     for (let idx in points) {
         console.log(idx)
         let point = points[idx]
-        __center_point.x = point.x
-        __center_point.y = point.y
+        //__center_point.x = point.x
+        //__center_point.y = point.y
         draw_circle(point, 5, "blue")
     }
 }
@@ -105,6 +107,11 @@ function calc_distance_px(p0, p1) {
         Math.pow( Math.abs(p0.x - p1.x), 2) +
         Math.pow( Math.abs(p0.y - p1.y), 2)
     )
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180)
+
 }
 
 function get_mouse_coordinate(e) {
@@ -143,6 +150,9 @@ function on_selecting_edge_at(xy) {
 
     console.log("edge: ", __config.edge)
 
+    __edge_point.x = __config.edge.xy.x
+    __edge_point.y = __config.edge.xy.y
+
     redraw_canvas()
     __status = Status.NONE
 }
@@ -166,14 +176,100 @@ function calculate_db_at_xy(xy) {
 
 }
 
-function on_selecting_point_at(xy) {
-    console.log("point selected: ", xy)
-    let new_point = {
-        x: xy.x,
-        y: xy.y
+function calculate_perp_point(line_start, line_end, point) {
+    let perp_point = {
+        x: 0,
+        y: 0
+    }
+    if (line_start.x == line_end.x) { // line is vertical
+        console.log("vertical")
+        perp_point.x = line_start.x
+        perp_point.y = point.y
+    } else if (line_start.y == line_end.y) { // line is horizontal
+        console.log("horizontal")
+        perp_point.x = point.x
+        perp_point.y = line_start.y
+    } else {
+        console.log("angled", line_end, line_start, point)
+        let m1 = (line_end.y - line_start.y) / (line_end.x - line_start.x)
+        let b1 = line_start.y - (m1 * line_start.x)
+        let m2 = -1.0 / m1;
+        let b2 = point.y - (m2 * point.x)
+
+        console.log(m1, b1, m2, b2)
+
+        perp_point.x = (b2 - b1) / (m1 - m2)
+        perp_point.y = (b2 * m1 - b1 * m2) / (m1 - m2)
     }
 
-    __selected_points.push(new_point)
+    return perp_point
+}
+
+function on_selecting_point_at(xy) {
+    console.log("point selected: ", xy)
+    let x = xy.x
+    let y = xy.y
+    let deg = __current_deg
+    let rad = deg2rad(deg)
+    let current_edge = {
+        x: 0,
+        y: 0,
+        db: -99,
+    }
+    let center_point = __config.center.xy
+    current_edge.x = (__config.edge.xy.x - center_point.x) * Math.cos(rad) - (__config.edge.xy.y - center_point.y) * Math.sin(rad) + center_point.x
+    current_edge.y = (__config.edge.xy.x - center_point.x) * Math.sin(rad) + (__config.edge.xy.y - center_point.y) * Math.cos(rad) + center_point.y
+    console.log("current_edge", current_edge)
+
+    let prep_point = calculate_perp_point(
+        center_point,
+        current_edge,
+        xy
+    )
+
+    console.log("prep_point for deg:", deg, prep_point)
+
+    let distance = calc_distance_px(center_point, prep_point)
+    let full_distance = calc_distance_px(center_point, __config.edge.xy)
+    let full_db = Math.abs(__config.edge.db - __config.center.db)
+    let db = full_db * (distance / full_distance) + __config.center.db
+    console.log("distance,full,full_db, db", distance, full_distance,full_db, db)
+    console.log("db => ", db)
+
+    prep_point.db = db
+
+    //let new_point = {
+    //    x: xy.x,
+    //    y: xy.y
+    //}
+    //__selected_points.push(new_point)
+    __selected_points.push(prep_point)
+
+    __current_deg += 10
+    if (__current_deg > 180) {
+        __status = Status.NONE;
+    }
+}
+
+function print_output_to_textarea(textarea_id) {
+    let str = "";
+    let obj = {
+        base: $("#text-base-model").val(),
+        key: "KEY",
+        name: $("#text-display-name").val(),
+        peak_db: 0,
+        resolution: 10,
+        bias_map: []
+    }
+
+    for (let idx = 0; idx < __selected_points.length; idx++) {
+        let db_floor2 = Math.round(__selected_points[idx].db * 100) / 100
+        obj.bias_map.push(db_floor2)
+    }
+
+    str = JSON.stringify(obj, null, 2)
+
+    $(textarea_id).val(str)
 }
 
 function canvas_click(e) {
@@ -242,6 +338,20 @@ function start_selecting_point(e) {
 
 function end_selecting_point(e) {
     __status = Status.NONE
+
+    print_output_to_textarea("#textarea-result")
+}
+
+function copy_textarea(e) {
+    let text = $("#textarea-result").val()
+    if (navigator.clipboard) {
+        return navigator.clipboard.writeText(text).then(function() {
+            console.log("copied")
+        })
+    } else {
+        $("#textarea-result").select()
+        document.execCommand("copy")
+    }
 }
 
 $("#canvas-ant").click(function(e) { canvas_click(e) })
@@ -251,3 +361,4 @@ $("#button-select-center").click(function(e) { selecting_center(e) })
 $("#button-select-edge").click(function(e) { selecting_edge(e) })
 $("#button-start-point").click(function(e) { start_selecting_point(e) })
 $("#button-end-point").click(function(e) { end_selecting_point(e) })
+$("#button-copy").click(function(e) { copy_textarea()})
